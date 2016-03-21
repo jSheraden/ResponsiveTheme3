565745b9b683b73f42f27a3a4c6b4f0bac24cd90
jsheraden <josasheraden@gmail.com>
2016-03-21 11:19:55 +0000

    Added .btn.btn-primary class to comment submit button

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
   $('#copyright').html('&copy ' + new Date().getFullYear());
+  $('input#submit').addClass('btn btn-primary');
 });
-
-/*
-$( document ).ready( function() {
-	jQuery.event.add( window, 'load', resizeFrame );
-	jQuery.event.add( window, 'resize', resizeFrame );
-
-	function resizeFrame() {
-		var hideElement = {
-			visibility: 'hidden',
-			position: 'absolute',
-			clear: 'both'
-		};
-		
-		var showElement = {
-			visibility: 'visible',
-			position: 'relative',
-			clear: 'none'
-		};
-		
-		var hideIfSmallScreen = $( window ).width() < 970 ? hideElement : showElement;
-		var showIfSmallScreen = $( window ).width() < 970 ? showElement : hideElement;
-		
-		$( '.sidebar' ).css( hideIfSmallScreen );
-	}
-} );
-*/
